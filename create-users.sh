fef124beb485375437541a5d59c6436dbcc95598
Dejan <55488175+dejandro@users.noreply.github.com>
2023-10-25 13:07:18 +0200

    Create create-users.sh

---
+++ create-users.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+accounts=$(cat usernames.txt)
+
+for i in $accounts;
+   do
+#     echo $i   #print usernames while creating accounts
+      sudo useradd -m -s /bin/bash "$i"
+      echo "$i:changePASSWORD!" | sudo chpasswd
+      sudo chown -R "$i:$i" "/home/$i"
+      sudo chmod 700 "/home/$i"
+done
+
+echo -e "\nResults: \n"
+sudo cat /etc/passwd | grep -i "$i*"
+
+#sudo group request
+echo ""
+while true; do
+     read -p "Add users to the sudo group? [Y/n]" input
+     case $input in
+        [yY]* )  for i in $accounts; do sudo usermod -aG sudo "$i"; done
+                 echo -e "\nUsers are added to the sudo group"; break;;
+        [nN]* )  echo -e "\nUsers will not be added to the sudo group"; break;;
+        * ) echo -e "\nInvalid response";;
+     esac
+done
+echo ""
+for i in $accounts; do id $i; done
